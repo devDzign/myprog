2a36530aee8c5d07b1b35e37a4699137aacf194c
mchabour <mourad.chabour@services.inter-invest.fr>
2022-10-12 14:55:19 +0200

    passed argument to template

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -12,6 +12,18 @@ type Repository struct {
 	App *config.AppConfig
 }
 
+// TemplateData holds data sent from handlers to templates
+type TemplateData struct {
+	StringMap map[string]string
+	IntMap    map[string]int
+	FloatMap  map[string]float32
+	Data      map[string]interface{}
+	CRFToken  string
+	Flash     string
+	Warning   string
+	Error     string
+}
+
 // NewRepo creates a new repositories
 func NewRepo(a *config.AppConfig) *Repository {
 
@@ -26,9 +38,11 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, request *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+	render.RenderTemplate(w, "home.page.html", &TemplateData{})
 }
 
 func (m *Repository) About(w http.ResponseWriter, request *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+	var stringMap map[string]string
+	stringMap["test"] = "Hello, again."
+	render.RenderTemplate(w, "about.page.html", &TemplateData{StringMap: stringMap})
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"path/filepath"
 	"webHello/pkg/config"
+	"webHello/pkg/handlers"
 )
 
 var functions = template.FuncMap{}
@@ -17,7 +18,7 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *handlers.TemplateData) {
 
 	var tc map[string]*template.Template
 	if app.UseCache {
@@ -34,7 +35,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	}
 
 	buf := new(bytes.Buffer)
-	err := t.Execute(buf, nil)
+	err := t.Execute(buf, td)
 
 	if err != nil {
 		log.Fatal(err)
